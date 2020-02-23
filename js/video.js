a420c839016cf656a9ba92a668c300ea375aaf5f
Drew-Pappas <41135406+Drew-Pappas@users.noreply.github.com>
2020-02-22 19:11:55 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,71 @@
+var video;
+
+function getVid(){
+	video = document.querySelector("#myVideo"); 
+}
+
+function playVid() { 
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+	console.log("Play Video");
+} 
+
+function pauseVid() {
+	video.pause() 
+	console.log("Pause Video");
+} 
+
+function decreaseSpeed() { 
+	video.playbackRate *= 0.8;
+  	console.log("Speed is " + video.playbackRate);
+} 
+
+function increaseSpeed() {
+	video.playbackRate *= 1.25;
+
+	console.log("Speed is " + video.playbackRate);
+} 
+
+function skipAhead() {
+	console.log("Location before jump " + video.currentTime);
+	if (video.currentTime + 60 < video.duration){
+		video.currentTime += 60;
+	} else {
+		video.currentTime = 0;
+		video.playbackRate = 1;
+	}
+	console.log("Current location is " + video.currentTime);
+	
+} 
+
+function mute() { 
+	if (video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute"
+		console.log("Unmuted");
+
+	} else {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute"
+		console.log("Muted");
+	}
+  	
+}
+
+function changeVolume() {
+	video.volume = document.querySelector("#volumeSlider").value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+	console.log("Volume is " + video.volume * 100 + "%"); 
+}
+       
+
+function gray() { 
+	// document.querySelector("#myVideo").classList.add(".grayscale"); // IDK why this isn't working
+	document.querySelector("#myVideo").style.filter = "grayscale(100%)";
+	console.log("In grayscale")
+}
+
+function color() {
+	document.querySelector("#myVideo").style.filter = "none";
+	console.log("In color") 
+}
